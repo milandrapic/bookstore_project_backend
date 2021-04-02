c8edea176e6b9c9a2b1474457aa2f9065213520d
milandrapic <55461949+milandrapic@users.noreply.github.com>
2021-04-02 05:42:15 -0400

    Delete bookstore directory

--- bookstore/src/main/java/com/bookclub/bookstore/model/Authority.java
+++
@@ -1,8 +0,0 @@
-package com.bookclub.bookstore.model;
-
-public class Authority {
-	
-	public static final String[] USER_AUTHORITIES = {"read","checkout"};
-	public static final String[] ADMIN_AUTHORITIES = {"read","checkout","report"};
-
-}
